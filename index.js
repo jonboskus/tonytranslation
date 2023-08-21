666d9082932b08912997503945591109fa746c71
jonboskus <111882320+jonboskus@users.noreply.github.com>
2023-08-21 10:57:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const navSlide = () => {
+
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.list');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+}
+
+
+navSlide();
